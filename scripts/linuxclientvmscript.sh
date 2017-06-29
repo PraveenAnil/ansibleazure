cddae8748d0859ae18fbe618ca8c61c68ebb51bc
PraveenAnil <praveendosth@gmail.com>
2017-06-29 22:52:32 +0530

    Update linuxclientvmscript.sh

--- scripts/linuxclientvmscript.sh
+++ scripts/linuxclientvmscript.sh
@@ -8,8 +8,8 @@
 echo "$1" "ALL = (ALL) NOPASSWD:ALL" >> /etc/sudoers
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -Uvh epel-release-latest-7*.rpm 
- 
-
+systemctl stop firewalld
+systemctl disable firewalld
 
 
 exit 0
